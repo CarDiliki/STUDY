db0f32f7d6f546f9aaed133f0488edd30d446487
Kafffer <1165795910@qq.com>
2023-11-11 01:26:49 +0800

    extract common function and encapsulate with template function

--- common.h
+++ common.h
@@ -4,9 +4,28 @@
 
 #ifndef STUDY4MYSELF_COMMON_H
 #define STUDY4MYSELF_COMMON_H
+#include <sstream>
 #include "include/Valid Anagram.h"
 #include "include/IntersectionOfTwoArrays.h"
 #include "include/TwoSum.h"
+
+
+
+//template function are more convenient and can handle various situations
+template<typename Input, typename Output, typename Delimiter>
+void splitString(const Input& input, Output& output, const Delimiter& delimiter){
+    stringstream ss(input);
+    string item;
+    while(getline(ss, item, delimiter)){
+        istringstream converter(item);
+        //template function define the container type value like this
+        typename Output::value_type value;
+        converter >> value;
+        output.push_back(value);
+    }
+}
+
+
 
 
 #endif //STUDY4MYSELF_COMMON_H
--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include "common.h"
-#include <sstream>
 
 
 //2023/11/8
 //242.Valid anagram
-//动脑子，别只想着暴力解决。。。
-void valid_anagram(){
+//use your brain, don't just think of violent solutions!!!!!
+void validAnagram(){
     string word_a, word_b;
     cout << "please enter the first string : ";
     cin >> word_a;
@@ -18,31 +17,28 @@ void valid_anagram(){
 //2023/11/8
 //349.Intersection Of Two Arrays
 //在限制了数值范围的情况下就不需要用set
+//when the range of value is limited, there is no need to use 'set'.
 //set占用空间比数组大，且速度更慢，set把数值映射到key上都要做hash计算的
-void intersection_ot_two_arrays(){
+//'set' takes up more space than arrays and is slower
+//'set' has to do hash calculations when mapping values to key
+void intersectionOfTwoArrays(){
     vector<int> nums_a, nums_b, result;
     string temp1, temp2;
     cout << "please enter the first array , separated by ',' : ";
     cin >> temp1;
     cout << "please enter the second array , separated by ',' : ";
     cin >> temp2;
-    stringstream nums1(temp1), nums2(temp2);
-    string a, b;
-    while (getline(nums1, a, ',')){
-        nums_a.push_back(stoi(a));
-        a.clear();
-    }
-    while (getline(nums2, b, ',')){
-        nums_b.push_back(stoi(b));
-        b.clear();
-    }
+    splitString(temp1, nums_a, ',');
+    splitString(temp2, nums_b, ',');
     result = IntersectionOfTwoArrays(nums_a, nums_b).intersection();
     for(auto &it : result){
         cout << it << " ";
     }
 }
 
-void two_sum(){
+//2023/11/9
+//1.Two Sum
+void twoSum(){
     vector<int> nums_a;
     string temp1;
     int target;
@@ -50,12 +46,7 @@ void two_sum(){
     cin >> temp1;
     cout << "please enter the target : ";
     cin >> target;
-    stringstream nums1(temp1);
-    string a;
-    while (getline(nums1, a, ',')){
-        nums_a.push_back(stoi(a));
-        a.clear();
-    }
+    splitString(temp1, nums_a, ',');
     TwoSum(nums_a, target).twoSum();
 }
 
@@ -65,9 +56,10 @@ void two_sum(){
 
 int main() {
 
-//    valid_anagram();
-//    intersection_ot_two_arrays();
-    two_sum();
+//    validAnagram();
+//    intersectionOfTwoArrays();
+//    twoSum();
+
 
     return 0;
 }
